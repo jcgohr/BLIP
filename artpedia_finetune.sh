d32501c0f40d7f1a4cc7a636276a6a1a9b1f1490
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-04 16:03:15 -0500

    attempt to convert nproc_per_node to int

--- artpedia_finetune.sh
+++ artpedia_finetune.sh
@@ -1,3 +1,3 @@
-python -m torch.distributed.run --nproc_per_node=$0 train_retrieval.py \
+python -m torch.distributed.run --nproc_per_node=$($0+0) train_retrieval.py \
 --config $1 \
 --output_dir $2
